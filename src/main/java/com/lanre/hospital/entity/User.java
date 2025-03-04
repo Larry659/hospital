e783d53c2ab19efbdb38270936bbe14146773db6
lanre <larryfunmbi26@gmail.com>
2025-03-04 12:05:09 +0100

    first commit

---
+++ src/main/java/com/lanre/hospital/entity/User.java
@@ -0,0 +1,25 @@
+package com.lanre.hospital.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "role", nullable = false)
+    private String role;
+
+}
